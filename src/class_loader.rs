e7cc22d38a5e93043c200f33872acb6f3944460b
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-09-08 22:20:35 +0900

    refactor: rename method

--- src/class_loader.rs
+++ src/class_loader.rs
@@ -7,7 +7,7 @@ pub struct ClassLoader {}
 impl ClassLoader {
     pub fn load_class(self, _class_name: &String) -> Vec<u8> {
         let filename = _class_name.to_owned() + ".class";
-        let class = match read_binary(filename) {
+        let class = match read_binary_file(filename) {
             Ok(class) => class,
             Err(_e) => panic!(
                 "Error: Can not find or read class {}\n Reason {}",
@@ -20,7 +20,7 @@ impl ClassLoader {
     }
 }
 
-fn read_binary(filename: String) -> Result<Vec<u8>, io::Error> {
+fn read_binary_file(filename: String) -> Result<Vec<u8>, io::Error> {
     let mut file = File::open(&filename)?;
     let metadata = fs::metadata(&filename)?;
     let mut buffer = vec![0; metadata.len() as usize];
