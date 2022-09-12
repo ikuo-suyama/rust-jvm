92b2f64628dcbb3b896a296f26d844a45fb0017d
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-09-12 21:29:33 +0900

    feat: read attributes

--- src/java_class.rs
+++ src/java_class.rs
@@ -39,7 +39,7 @@ impl ClassFile {
         let interfaces = parse_interfaces(&mut cursor, interfaces_count);
         let fields_count: u16 = read_u16(&mut cursor);
         let fields = parse_fields(&mut cursor, fields_count);
-        let methods_count: u16;
+        let methods_count: u16 = read_u16(&mut cursor);
         // let methods[methods_count]: method_info;
         let attributes_count: u16;
         // let attributes[attributes_count]: attribute_info;
@@ -77,17 +77,100 @@ pub struct FieldInfo {
     attributes: Vec<AttributeInfo>,
 }
 
+pub struct MethodInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes_count: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
 pub struct AttributeInfo {
     attribute_name_index: u16,
     attribute_length: u32,
     info: Vec<u8>,
 }
 
-fn parse_fields(cursur: &mut Cursor<&[u8]>, fields_count: u16) -> Vec<FieldInfo> {
+fn parse_fields(cursor: &mut Cursor<&[u8]>, fields_count: u16) -> Vec<FieldInfo> {
     if fields_count > 1 {
         panic!("fileds are not implemented yet");
     }
     vec![]
+}
+
+fn parse_methods(cursor: &mut Cursor<&[u8]>, methods_count: u16) -> Vec<MethodInfo> {
+    let mut methods: Vec<MethodInfo> = vec![];
+    for _ in 0..methods_count {}
+    methods
+}
+
+fn parse_method(cursor: &mut Cursor<&[u8]>) -> MethodInfo {
+    let access_flags: u16 = read_u16(cursor);
+    let name_index: u16 = read_u16(cursor);
+    let descriptor_index: u16 = read_u16(cursor);
+    let attributes_count: u16 = read_u16(cursor);
+    let attributes = parse_attributes(cursor, attributes_count);
+
+    MethodInfo {
+        access_flags,
+        name_index,
+        descriptor_index,
+        attributes_count,
+        attributes,
+    }
+}
+
+fn parse_attributes(cursor: &mut Cursor<&[u8]>, attributes_count: u16) -> Vec<AttributeInfo> {
+    let mut attributes: Vec<AttributeInfo> = vec![];
+    for _ in 0..attributes_count {
+        attributes.push(parse_attribute_info(cursor))
+    }
+    attributes
+}
+
+fn parse_attribute_info(cursor: &mut Cursor<&[u8]>) -> AttributeInfo {
+    let attribute_name_index = read_u16(cursor);
+    let attribute_length = read_u32(cursor);
+    AttributeInfo {
+        attribute_name_index,
+        attribute_length,
+        info: vec![],
+    }
+}
+
+#[test]
+fn test_parse_attribute_info() {
+    let bytes: &[u8] = &[
+        // Attributes
+        0x00, 0x19, 0x00, 0x00, 0x00, 0x26, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0a, 0xb2,
+        0x00, 0x07, 0xb8, 0x00, 0x0d, 0xb6, 0x00, 0x13, 0xb1, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1a,
+        0x00, 0x00, 0x00, 0x0a, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03, 0x00, 0x09, 0x00, 0x04,
+    ];
+    let mut cursor = Cursor::new(bytes);
+
+    let result = parse_attribute_info(&mut cursor);
+
+    assert_eq!(result.attribute_name_index, 0x0019);
+    assert_eq!(result.attribute_length, 0x00000026);
+}
+
+#[test]
+fn test_parse_method() {
+    let bytes: &[u8] = &[
+        0x00, 0x09, 0x00, 0x1b, 0x00, 0x1c, 0x00, 0x01, // Attributes
+        0x00, 0x19, 0x00, 0x00, 0x00, 0x26, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0a, 0xb2,
+        0x00, 0x07, 0xb8, 0x00, 0x0d, 0xb6, 0x00, 0x13, 0xb1, 0x00, 0x00, 0x00, 0x01, 0x00, 0x1a,
+        0x00, 0x00, 0x00, 0x0a, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03, 0x00, 0x09, 0x00, 0x04,
+    ];
+    let mut cursor = Cursor::new(bytes);
+
+    let result = parse_method(&mut cursor);
+
+    assert_eq!(result.access_flags, 0x09);
+    assert_eq!(result.name_index, 0x1b);
+    assert_eq!(result.descriptor_index, 0x1c);
+    assert_eq!(result.attributes_count, 0x01);
+    assert_eq!(result.attributes.len(), result.attributes_count as usize);
 }
 
 #[test]
@@ -108,8 +191,10 @@ fn test_parse_class() {
     assert_eq!(result.access_flags, 0x21);
     assert_eq!(result.this_class, 14_u16);
     assert_eq!(result.super_class, 02_u16);
+
     assert_eq!(result.interfaces_count, 0);
     assert_eq!(result.interfaces.len(), result.interfaces_count as usize);
+
     assert_eq!(result.fields_count, 0);
     assert_eq!(result.fields.len(), result.fields_count as usize);
 }
