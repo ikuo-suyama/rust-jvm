a73ff147368585b05b0ef2439b7208c392bdfcaf
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-09-18 10:13:35 +0900

    refactor: separete instruction

--- src/thread.rs
+++ src/thread.rs
@@ -32,9 +32,11 @@ impl<'a> Thread<'a> {
 
     pub fn run(mut self, context: &'a Class, current_method: &'a MethodInfo) {
         let mut frame = Frame::create(context, current_method);
+
         self.java_virtual_machine_stack.push(frame);
         let top = self.java_virtual_machine_stack.len() - 1;
         let frame = self.java_virtual_machine_stack.get_mut(top).unwrap();
+
         frame.invoke();
     }
 }
@@ -59,80 +61,81 @@ impl<'a> Frame<'a> {
         }
     }
 
-    pub fn invoke(&self) -> u8 {
+    pub fn invoke(&mut self) {
         let code = &self.current_method.get_code_attribute().code;
-        self._invoke(code)
+        _invoke(self, code);
     }
-    fn _invoke(&self, code: &Vec<u8>) -> u8 {
-        let mut pc = 0;
-        let mut local_variable: Vec<u8> = vec![0, 0, 0];
-        let mut operand_stack: Vec<u8> = Vec::new();
+}
 
-        let result = loop {
-            let instruction = code[pc];
-            println!("[DEBUG] -- pc: {} instruction: {:#04x}", pc, instruction);
-            match instruction {
-                ICONST_1 => {
-                    operand_stack.push(1);
-                    pc += 1;
-                }
-                ICONST_2 => {
-                    operand_stack.push(2);
-                    pc += 1;
-                }
+fn _invoke(frame: &mut Frame, code: &Vec<u8>) -> u8 {
+    let mut pc = 0;
+    let mut local_variable: Vec<u8> = vec![0, 0, 0];
+    let mut operand_stack: Vec<u8> = Vec::new();
 
-                ISTORE_0 => {
-                    let val = operand_stack.pop().unwrap();
-                    local_variable[0] = val;
-                    pc += 1;
-                }
-                ISTORE_1 => {
-                    let val = operand_stack.pop().unwrap();
-                    local_variable[1] = val;
-                    pc += 1;
-                }
-                ISTORE_2 => {
-                    let val = operand_stack.pop().unwrap();
-                    local_variable[2] = val;
-                    pc += 1;
-                }
+    let result = loop {
+        let instruction = code[pc];
+        println!("[DEBUG] -- pc: {} instruction: {:#04x}", pc, instruction);
+        match instruction {
+            ICONST_1 => {
+                operand_stack.push(1);
+                pc += 1;
+            }
+            ICONST_2 => {
+                operand_stack.push(2);
+                pc += 1;
+            }
 
-                ILOAD_0 => {
-                    let val = local_variable[0];
-                    operand_stack.push(val);
-                    pc += 1;
-                }
-                ILOAD_1 => {
-                    let val = local_variable[1];
-                    operand_stack.push(val);
-                    pc += 1;
-                }
-                ILOAD_2 => {
-                    let val = local_variable[2];
-                    operand_stack.push(val);
-                    pc += 1;
-                }
+            ISTORE_0 => {
+                let val = operand_stack.pop().unwrap();
+                local_variable[0] = val;
+                pc += 1;
+            }
+            ISTORE_1 => {
+                let val = operand_stack.pop().unwrap();
+                local_variable[1] = val;
+                pc += 1;
+            }
+            ISTORE_2 => {
+                let val = operand_stack.pop().unwrap();
+                local_variable[2] = val;
+                pc += 1;
+            }
 
-                IADD => {
-                    let val1 = operand_stack.pop().unwrap();
-                    let val2 = operand_stack.pop().unwrap();
+            ILOAD_0 => {
+                let val = local_variable[0];
+                operand_stack.push(val);
+                pc += 1;
+            }
+            ILOAD_1 => {
+                let val = local_variable[1];
+                operand_stack.push(val);
+                pc += 1;
+            }
+            ILOAD_2 => {
+                let val = local_variable[2];
+                operand_stack.push(val);
+                pc += 1;
+            }
 
-                    let result = val1 + val2;
-                    operand_stack.push(result);
-                    pc += 1;
-                }
+            IADD => {
+                let val1 = operand_stack.pop().unwrap();
+                let val2 = operand_stack.pop().unwrap();
 
-                IRETURN => {
-                    let val = operand_stack.pop().unwrap();
-                    break val;
-                }
-                _ => panic!("Instruction 0x{:x} doesn't implement yet", instruction),
-            };
+                let result = val1 + val2;
+                operand_stack.push(result);
+                pc += 1;
+            }
+
+            IRETURN => {
+                let val = operand_stack.pop().unwrap();
+                break val;
+            }
+            _ => panic!("Instruction 0x{:x} doesn't implement yet", instruction),
         };
+    };
 
-        println!("Hello, jvm! the result is: {}", result);
-        result
-    }
+    println!("Hello, jvm! the result is: {}", result);
+    result
 }
 
 #[test]
@@ -140,9 +143,9 @@ fn test_invoke() {
     let code: Vec<u8> = vec![0x04, 0x3C, 0x05, 0x3D, 0x1B, 0x1C, 0x60, 0xAC];
     let class = frame_test::dummy_class();
     let method = frame_test::dummy_method();
-    let frame = Frame::create(&class, &method);
+    let mut frame = Frame::create(&class, &method);
 
-    let result = frame._invoke(&code);
+    let result = _invoke(&mut frame, &code);
 
     assert_eq!(result, 3);
 }
