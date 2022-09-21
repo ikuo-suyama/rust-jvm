a41061dcf5e3556595878b38a8ca12edc8122565
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-09-21 22:38:59 +0900

    refactor: add RC for method

--- src/class.rs
+++ src/class.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use crate::class_attributes::{FieldInfo, MethodInfo};
 
 #[derive(Debug)]
 pub struct Class {
     pub descriptor: String,
-    pub methods: HashMap<String, MethodInfo>,
-    pub fields: HashMap<String, FieldInfo>,
+    pub methods: HashMap<String, Rc<MethodInfo>>,
+    pub fields: HashMap<String, Rc<FieldInfo>>,
 }
