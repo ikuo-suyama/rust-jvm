9bdc9871a0bc7121aa618b7d1a559f1986dd7b13
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2022-10-05 21:16:26 +0900

    add: types java class

---
+++ java/Types.java
@@ -0,0 +1,31 @@
+public class Types {
+    public static void main(String[] args) {
+        int i = 1;
+        int i2 = 2;
+        int i3 = i + i2;
+
+        long l = 1;
+        long l2 = 2;
+        long l3 = l + l2;
+
+        boolean b = true;
+        boolean b2 = false;
+        boolean b3 = b | b2;
+
+        short s = 1;
+        short s2 = 2;
+        short s3 = (short)(s + s2);
+
+        char c = 'a';
+        char c2 = 'b';
+        char c3 = (char)(c + c2);
+
+        float f = 1.01f;
+        float f2 = 2.01f;
+        float f3 = f + f2;
+
+        double d = 1.001;
+        double d2 = 2.002;
+        double d3 = d + d2;
+    }
+}
